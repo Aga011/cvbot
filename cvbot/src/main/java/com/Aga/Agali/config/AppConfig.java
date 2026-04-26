6dc00bf0a3785e997ccef1fb9415bbe3bb55951a
Ağa Ağalı <agaagali633.19@gmail.com>
2026-04-26 23:01:34 +0400

    fix mvnw permission

--- cvbot/src/main/java/com/Aga/Agali/config/AppConfig.java
+++ cvbot/src/main/java/com/Aga/Agali/config/AppConfig.java
@@ -2,16 +2,8 @@ package com.Aga.Agali.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
-
-
-
-
 import org.springframework.context.annotation.Configuration;
-
-
-
 import org.springframework.web.client.RestTemplate;
-
 @Configuration
 public class AppConfig {
 
